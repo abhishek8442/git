6611b27199b2e9b25c12a11e55da651338798edb
abhishek8442 <116899106+abhishek8442@users.noreply.github.com>
2022-11-04 13:30:48 +0530

    notice related and password update

---
+++ IndiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apply.india;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
